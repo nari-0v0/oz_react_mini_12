3483360830c1ba2d41dfb8d811e45c477bbfc588
nari-0v0 <ej9811@naver.com>
2025-07-28 14:20:19 +0900

    검색기능 구현

--- movie/src/App.jsx
+++ movie/src/App.jsx
@@ -1,42 +1,56 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import MovieCard from './components/MovieCard';
+import { useSearchParams } from 'react-router-dom';
 
 export default function App() {
   const [movies, setMovies] = useState([]);
+  const [searchParam] = useSearchParams();
+  const [loading, setLoading] = useState(true);
+  const query = searchParam.get('query');
 
   useEffect(() => {
     const fetchMovies = async () => {
-      const res = await fetch(
-        'https://api.themoviedb.org/3/movie/popular?language=ko-KR',
-        {
-          headers: {
-            accept: 'application/json',
-            Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_TOKEN}`,
-          },
-        }
-      );
+      setLoading(true);
+      const endpoint = query
+        ? `https://api.themoviedb.org/3/search/movie?query=${query}&language=ko-KR`
+        : `https://api.themoviedb.org/3/movie/popular?language=ko-KR`;
+
+      const res = await fetch(endpoint, {
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_TOKEN}`,
+        },
+      });
+
       const data = await res.json();
       const filtered = data.results.filter((movie) => !movie.adult);
       setMovies(filtered);
+      setLoading(false);
     };
 
     fetchMovies();
-  }, []);
+  }, [query]);
+
+  if (loading) return <div className="loading">로딩 중...</div>;
 
   return (
     <main>
-      <ul className="movie-container">
-        {movies.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            id={movie.id}
-            title={movie.title}
-            poster_path={movie.poster_path}
-            vote_average={movie.vote_average}
-          />
-        ))}
-      </ul>
+      {!loading && movies.length === 0 ? (
+        <div className="no-results">검색 결과가 없습니다.</div>
+      ) : (
+        <ul className="movie-container">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              id={movie.id}
+              title={movie.title}
+              poster_path={movie.poster_path}
+              vote_average={movie.vote_average}
+            />
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
--- movie/src/components/NavBar.jsx
+++ movie/src/components/NavBar.jsx
@@ -1,8 +1,22 @@
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import useDebounce from '../hooks/useDebounce';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import './NavBar.css';
-import GenreSelect from './GenreSelect';
 
 export default function NavBar() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const debouncedSearch = useDebounce(searchTerm, 500);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (debouncedSearch) {
+      navigate(`/?query=${encodeURIComponent(debouncedSearch)}`);
+    } else if (location.pathname === '/') {
+      navigate('/');
+    }
+  }, [debouncedSearch]);
+
   return (
     <nav className="navbar">
       <Link to="/" className="logo">
@@ -10,7 +24,13 @@ export default function NavBar() {
         <span className="movie">무비</span>
       </Link>
 
-      <input className="search" type="text" placeholder="영화를 검색하세요" />
+      <input
+        className="search"
+        type="text"
+        placeholder="영화를 검색하세요"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value || '')}
+      />
 
       <div className="auth-buttons">
         <button className="btn login">로그인</button>
